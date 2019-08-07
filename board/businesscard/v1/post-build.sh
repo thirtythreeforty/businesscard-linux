2aa0662fcc4b44059ffb97fe4eb4dd2f221a1d78
George Hilliard <thirtythreeforty@gmail.com>
2019-08-07 01:03:56 -0500

    Implement writable /root for users to keep data

--- board/businesscard/v1/post-build.sh
+++ board/businesscard/v1/post-build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 grep -q "GADGET_SERIAL" "${TARGET_DIR}/etc/inittab" \
 	|| echo '/dev/ttyGS0::respawn:/sbin/getty -L  /dev/ttyGS0 0 vt100 # GADGET_SERIAL' >> "${TARGET_DIR}/etc/inittab"
+grep -q "ubi0:persist" "${TARGET_DIR}/etc/fstab" \
+	|| echo 'ubi0:persist	/root		ubifs	defaults	0	0' >> "${TARGET_DIR}/etc/fstab"
